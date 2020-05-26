7a19c1673be6ef782aa4f7fa5e332ecf96202e87
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-26 21:13:48 +0530

    type checking with function parameters

--- index.ts
+++ index.ts
@@ -3,27 +3,29 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
 interface Todo {
-    id: number;
-    title: string;
-    completed: boolean; 
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
 axios
   .get(url)
   .then((response) => {
-
     const todo = response.data as Todo;
 
-    const id= todo.id;
-    const title= todo.title;
-    const finished= todo.completed;
-
-
-
-    console.log(`
-    The Requested Todo-id is: ${id}
-    The Requested Todo-title is: ${title}
-    is it finished: ${finished }
-    `);
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    // logTodo(id,title,completed);
+    // calling logTodo with wrong sequence of parameters
+    logTodo(id, title, completed);
   })
   .catch((err) => console.log(err));
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`s
+    The Requested Todo-id is: ${id}
+    The Requested Todo-title is: ${title}
+    is it finished: ${completed}
+    `);
+};
