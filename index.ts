ad12e5c3622b63beb248bd19300096c6df6ef3e0
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-26 13:46:01 +0530

    first app using typeScript and tsNode

---
+++ index.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+axios
+  .get(url)
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => console.log(err));
